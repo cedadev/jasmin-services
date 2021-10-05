2541172d2cb22f4899dd95263a452c4c37bbfc7f
Alex Manning <alexander.manning@stfc.ac.uk>
2021-10-05 12:01:01 +0100

    Request_decide javascript now works as expected.
    Bootsrap forms now have a datepicker built in.

--- jasmin_services/static/jasmin_services/js/request_decide.js
+++ jasmin_services/static/jasmin_services/js/request_decide.js
@@ -1,32 +1,18 @@
-// Reset the hiding of form-groups except for those we are worried about
-$('.form-group').not('.reason, :has([name^="expires"]), :has([name$="_reason"])').show();
 // Constants used in the function below
 var YES = 'APPROVED', NO = 'REJECTED', NO_INCOMPLETE = 'INCOMPLETE', CUSTOM_DATE = '7';
 // This function makes sure only fields which are needed are shown
 var toggle_fields = function() {
     var state = $('[name="state"]').val();
     var expires = $('[name="expires"]').val();
-    $('[name="expires"]').closest('.form-group')[state == YES ? 'show' : 'hide']();
+    $('[name="expires"]').closest('.row')[state == YES ? 'show' : 'hide']();
     var show_custom = ( state == YES && expires == CUSTOM_DATE );
-    $('[name="expires_custom"]').closest('.form-group')[show_custom ? 'show' : 'hide']();
-    $('[name$="_reason"]').closest('.form-group')[( state == NO || state == NO_INCOMPLETE ) ? 'show' : 'hide']();
+    $('[name="expires_custom"]').closest('.row')[show_custom ? 'show' : 'hide']();
+    $('[name$="_reason"]').closest('.row')[( state == NO || state == NO_INCOMPLETE ) ? 'show' : 'hide']();
     $('[for="id_user_reason"]')[0].innerHTML = [( state == NO ) ? 'Reason for rejection (user)' : 'Reason for incomplete (user)'];
     $('[for="id_internal_reason"]')[0].innerHTML = [( state == NO ) ? 'Reason for rejection (internal)' : 'Reason for incomplete (internal)'];
 }
 toggle_fields();
 $('[name="state"], [name="expires"]').on('change', toggle_fields);
-
-// For date fields, replace the control with an input group
-$('input[type="date"]').each(function() {
-    var $input = $(this);
-    var $inputGroup = $('<div class="input-group"><span class="input-group-addon"><i class="fa fa-fw fa-calendar"></i></span></div>').insertBefore($input);
-    $input.attr('type', 'text').detach().prependTo($inputGroup);
-    $input.datepicker({
-        'format' : 'yyyy-mm-dd',
-        'startDate' : new Date(),
-        'autoclose' : true
-    });
-});
 
 // Replace the textareas with markdown editors
 $('textarea').each(function() {
@@ -42,4 +28,3 @@ $('textarea').each(function() {
     // Remove the help text as it is no longer required
     $(this).siblings('.help-block').find('.help-block:not(.error-block)').remove();
 })
-    </script>
