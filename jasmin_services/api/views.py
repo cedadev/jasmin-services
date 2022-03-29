43650c44a749d7d22b128449329b9a4c45ec917b
Alex Manning <alexander.manning@stfc.ac.uk>
2022-03-29 15:22:20 +0100

    Schema fix.

--- jasmin_services/api/views.py
+++ jasmin_services/api/views.py
@@ -3,16 +3,34 @@ import datetime as dt
 
 import django.shortcuts as djshortcuts
 import rest_framework.generics as rf_generics
+import rest_framework.schemas as rf_schemas
 
 from .. import models
 from . import serializers
 
 
 class ServiceRolesView(rf_generics.ListAPIView):
-    """List the roles users hold for a given service."""
+    """List all the roles users held by all the users of a given service."""
 
     serializer_class = serializers.ServiceRolesSerializer
     required_scopes = ["jasmin.services.serviceroles.all"]
+
+    schema = rf_schemas.openapi.AutoSchema()
+
+    # Override the schema.
+    # rest_framework does not infer the response shape correctly
+    # due to the custom ListSerializer.
+    schema.get_components = lambda _path, _method: {
+        "ServiceRoles": {
+            "type": "object",
+            "additionalProperties": {"type": "array", "items": {"type": "string"}},
+            "example": {
+                "fry": ["USER"],
+                "professor": ["USER", "MANAGER"],
+                "leela": ["USER"],
+            },
+        }
+    }
 
     def get_queryset(self):
         """Return valid users and roles for a given service."""
