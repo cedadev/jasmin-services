9f6996255ad6a4e106404ce5b6ee2e4a6a9b4301
Alex Manning <alexander.manning@stfc.ac.uk>
2022-12-19 12:52:26 +0000

    https://code.djangoproject.com/ticket/31180

--- jasmin_services/__init__.py
+++ jasmin_services/__init__.py
@@ -7,6 +7,3 @@ to join services.
 
 __author__ = "Matt Pryor"
 __copyright__ = "Copyright 2015 UK Science and Technology Facilities Council"
-
-
-default_app_config = "{}.apps.AppConfig".format(__name__)
