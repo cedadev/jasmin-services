321361fb58de3ed2cf6be10377d74d8b1ebdda14
Alex Manning <alexander.manning@stfc.ac.uk>
2023-09-18 16:44:21 +0100

    Add logging to keycloak behaviour.

--- jasmin_services/models/behaviours/keycloak.py
+++ jasmin_services/models/behaviours/keycloak.py
@@ -1,9 +1,13 @@
 """Behaviours to allow integration with KeyCloak."""
 
+import logging
+
 import django.conf
 import keycloak
 
 from .base import Behaviour
+
+logger = logging.getLogger(__name__)
 
 
 class KeycloakAttributeBehaviour(Behaviour):
@@ -25,12 +29,14 @@ class KeycloakAttributeBehaviour(Behaviour):
 
     def apply(self, user, role):
         """Add the user to the specified keycloak groups."""
+        logger.info("Applying keycloak group %s to user %s.", role.service.name, user.username)
         kc_group = self.keycloak.get_group_by_path(f"/{role.service.name}")
         kc_user_id = self.keycloak.get_user_id(user.username)
         self.keycloak.group_user_add(kc_user_id, kc_group.get("id"))
 
     def unapply(self, user, role):
         """Remove the user from the specified keycloak groups."""
+        logger.info("Removing keycloak group %s from user %s.", role.service.name, user.username)
         kc_group = self.keycloak.get_group_by_path(f"/{role.service.name}")
         kc_user_id = self.keycloak.get_user_id(user.username)
         self.keycloak.group_user_remove(kc_user_id, kc_group.get("id"))
