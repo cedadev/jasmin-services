10e649ffaa768a0d73c80165bac4592506c4b331
Alex Manning <alexander.manning@stfc.ac.uk>
2024-03-27 16:43:21 +0000

    Fix import problems.

--- jasmin_metadata/models/__init__.py
+++ jasmin_metadata/models/__init__.py
@@ -1,7 +1,55 @@
-"""
-Models for the JASMIN dynamic forms app.
-"""
+"""Models for the JASMIN dynamic forms app."""
 
 __author__ = "Matt Pryor"
 __copyright__ = "Copyright 2015 UK Science and Technology Facilities Council"
-from .forms import *
+
+from .base import HasMetadata, Metadatum
+from .forms import (
+    BooleanField,
+    ChoiceField,
+    ChoiceFieldBase,
+    DateField,
+    DateTimeField,
+    EmailField,
+    Field,
+    FloatField,
+    Form,
+    IntegerField,
+    IPv4Field,
+    MultiLineTextField,
+    MultipleChoiceField,
+    RegexField,
+    SingleLineTextField,
+    SlugField,
+    TextFieldBase,
+    TimeField,
+    URLField,
+    UserChoice,
+)
+
+__all__ = [
+    "HasMetadata",
+    "Metadatum",
+    "BooleanField",
+    "ChoiceField",
+    "ChoiceFieldBase",
+    "DateField",
+    "DateTimeField",
+    "EmailField",
+    "Field",
+    "FloatField",
+    "Form",
+    "HasMetadata",
+    "IPv4Field",
+    "IntegerField",
+    "Metadatum",
+    "MultiLineTextField",
+    "MultipleChoiceField",
+    "RegexField",
+    "SingleLineTextField",
+    "SlugField",
+    "TextFieldBase",
+    "TimeField",
+    "URLField",
+    "UserChoice",
+]
