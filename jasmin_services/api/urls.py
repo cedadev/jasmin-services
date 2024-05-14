ee3e8a2e8d9593a9313be41982ddce16d72d9044
Alex Manning <alexander.manning@stfc.ac.uk>
2024-05-14 11:47:43 +0100

    Get service roles using a nested viewset rather than an action.

--- jasmin_services/api/urls.py
+++ jasmin_services/api/urls.py
@@ -21,19 +21,32 @@ primary_router.register(
     apiviews.UsersViewSet,
     basename="user",
 )
+
+# Create a route for accesing service by id.
 primary_router.register(
     "v1/services",
     apiviews.ServicesViewSet,
     basename="service",
 )
+# Create a route for accessing roles under services.
+services_router = rest_framework_nested.routers.NestedDefaultRouter(
+    parent_router=primary_router,
+    parent_prefix="v1/services",
+    lookup="service",
+)
+services_router.register(
+    "roles",
+    apiviews.RolesNestedUnderServicesViewSet,
+    basename="services-roles",
+)
 
-# Create a router for accessing services by name.
+# Create a nested router for accessing services by name.
 primary_router.register(
     "v1/categories",
     apiviews.CategoriesViewSet,
     basename="category",
 )
-categories_router = rest_framework_nested.routers.NestedSimpleRouter(
+categories_router = rest_framework_nested.routers.NestedDefaultRouter(
     parent_router=primary_router,
     parent_prefix="v1/categories",
     lookup="category",
@@ -43,12 +56,33 @@ categories_router.register(
     apiviews.ServicesNestedUnderCategoriesViewSet,
     basename="category-services",
 )
+# Create a nested router for accessing roles under categories under services..
+categories_services_router = rest_framework_nested.routers.NestedDefaultRouter(
+    parent_router=categories_router,
+    parent_prefix="services",
+    lookup="service",
+)
+categories_services_router.register(
+    "roles",
+    apiviews.RolesNestedUnderServicesViewSet,
+    basename="category-services-roles",
+)
 
 # Combine all the nested routers into one.
 router = rf_routers.DefaultRouter()
 router.registry.extend(primary_router.registry)
 # Fiddle the routes to include the prefix from the parent.
 # Without this, the route is not nested properly.
+# Works around https://github.com/alanjds/drf-nested-routers/issues/292
+router.registry.extend(
+    [(f"{services_router.parent_regex}{x[0]}", x[1], x[2]) for x in services_router.registry]
+)
 router.registry.extend(
     [(f"{categories_router.parent_regex}{x[0]}", x[1], x[2]) for x in categories_router.registry]
+)
+router.registry.extend(
+    [
+        (f"{categories_services_router.parent_regex}{x[0]}", x[1], x[2])
+        for x in categories_services_router.registry
+    ]
 )
