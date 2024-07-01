b7e1092a466d0b8dd13e41f83b74aed63e0cf2ae
Alex Manning <alexander.manning@stfc.ac.uk>
2024-07-01 11:05:09 +0100

    Extend user_may_apply slightly.
    Make sure than when users recieve the reminder email, they are always
    able to reapply!

--- jasmin_services/models/role.py
+++ jasmin_services/models/role.py
@@ -153,7 +153,7 @@ class Role(models.Model):
                 Q(grant__revoked=False)  # Valid grants are not revoked.
                 & Q(grant__next_grant__isnull=True)  # filter only 'active' grants
                 & Q(
-                    grant__expires__gt=(django.utils.timezone.localdate() + dt.timedelta(days=60))
+                    grant__expires__gt=(django.utils.timezone.localdate() + dt.timedelta(days=65))
                 )  # Only include grants which don't expire in the next 60 days.
             )
             # And any pending requests.
