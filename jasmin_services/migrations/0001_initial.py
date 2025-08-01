62a165708de9123668744d82908afcdde8e81741
Alex Manning <alexander.manning@stfc.ac.uk>
2025-08-01 15:24:18 +0100

    Finish squashing migrations in jasmin_notifications.

--- jasmin_services/migrations/0001_initial.py
+++ jasmin_services/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         ("jasmin_metadata", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ("contenttypes", "0002_remove_content_type_name"),
-        ("jasmin_notifications", "0001_initial"),
+        ("jasmin_notifications", "0001_squashed_0004_alter_notificationtype_level"),
     ]
 
     operations = [
